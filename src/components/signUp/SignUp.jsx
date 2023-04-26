a8de49abea0206f567c63a0f8746f0dc1cb7e06a
robin <rcgaur84@gmail.com>
2023-04-26 20:17:15 +0530

    Multi step functionality

---
+++ src/components/signUp/SignUp.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import UserDetails from '../userDetails/UserDetails'
+import Privacy from '../privacy/Privacy'
+import Validation from '../validation/Validation'
+
+const SignUp = () => {
+    const [stateStep,setStateStep]=useState({
+        step:1, 
+        name:'',
+        phone:'',
+        dob:''
+    })
+    const {step}=stateStep
+    const {name, phone, dob}=stateStep
+    const values={name, phone, dob}
+
+
+    const prevStep=()=>{
+        const {step}=stateStep
+        setStateStep({step: step-1})
+
+    }
+
+    const nextStep=()=>{
+        const {step}=stateStep
+        setStateStep({step: step+1})
+
+    }
+
+    const handleChange=input=>(e)=>{
+        setStateStep({[input]: e.target.value})
+
+    }
+    console.log(step)
+
+    switch(step){
+        case 1: return(<UserDetails nextStep={nextStep} handleChange={handleChange} values={values}/>)
+        case 2: return(<Privacy prevStep={prevStep} nextStep={nextStep} handleChange={handleChange} values={values}/>)
+        case 3: return(<Validation prevStep={prevStep} nextStep={nextStep} handleChange={handleChange} values={values}/>)
+        default:
+    }
+}
+export default SignUp
